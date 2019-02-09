955f85e2eb7d7b3a07c179af9d25d95e13d45f53
Vijay-Mundaragi <saivijay010@gmail.com>
2019-02-09 18:24:33 +0530

    routing

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(reuest):
+	return HttpResponse("<h1> Blog Home </h1>")
+
+
+def about(reuest):
+	return HttpResponse("<h1> Blog About </h1>")
