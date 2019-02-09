5c4333ef3c86a5579d0838e5f2428c8047167f71
Vijay-Mundaragi <saivijay010@gmail.com>
2019-02-09 20:17:21 +0530

    templates, css, bootstrap, template-inheritance

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 # Create your views here.
 
-def home(reuest):
-	return HttpResponse("<h1> Blog Home </h1>")
+posts = [
+	{
+		"author": "Author 1",
+		"title": "Post 1",
+		"content": " The only time success comes before work is in the dictionary....",
+		"date_posted": "February 9, 2019"
+	},
+	{
+		"author": "Author 2",
+		"title": "Post 2",
+		"content": " I know people usually better than they know themselves",
+		"date_posted": "February 8, 2019"
+	}
+]
 
 
-def about(reuest):
-	return HttpResponse("<h1> Blog About </h1>")
+
+def home(request):
+	context = {
+		"posts": posts
+	}
+	return render(request, "blog/home.html", context)
+
+
+def about(request):
+	return render(request, "blog/about.html")
