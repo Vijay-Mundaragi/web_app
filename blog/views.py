67e1cf580cf101e85c8a5a8eb8708285e4e15ac2
Vijay-Mundaragi <saivijay010@gmail.com>
2019-02-10 18:21:38 +0530

    database and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+
 # Create your views here.
-
-posts = [
-	{
-		"author": "Author 1",
-		"title": "Post 1",
-		"content": " The only time success comes before work is in the dictionary....",
-		"date_posted": "February 9, 2019"
-	},
-	{
-		"author": "Author 2",
-		"title": "Post 2",
-		"content": " I know people usually better than they know themselves",
-		"date_posted": "February 8, 2019"
-	}
-]
-
-
 
 def home(request):
 	context = {
-		"posts": posts
+		"posts": Post.objects.all()
 	}
 	return render(request, "blog/home.html", context)
 
