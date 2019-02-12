b1bab7716e97a852dba70329a1365b41a88fc9c6
Vijay-Mundaragi <saivijay010@gmail.com>
2019-02-12 23:42:17 +0530

    profile update feature

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from PIL import Image
+
 # Create your models here.
 
 class Profile(models.Model):
@@ -10,4 +12,12 @@ class Profile(models.Model):
 	def __str__(self):
 		return f'{self.user.username} Profile'
 
+	def save(self):
+		super().save()
+		img = Image.open(self.image.path)
+		#resizing the image for performance also saves space in file system
+		if img.height > 300 or img.width > 300:
+			output_size = (300,300)
+			img.thumbnail(output_size)
+			img.save(self.image.path)
 
