28e1f12f510ef623bd20ffad8178702377a23e4c
Vijay-Mundaragi <saivijay010@gmail.com>
2019-02-17 12:59:33 +0530

    adding comments to post

--- blog/urls.py
+++ blog/urls.py
@@ -13,5 +13,9 @@ urlpatterns = [
     
     path('user/<str:username>', views.UserPostListView.as_view(), name="user-posts"),
 
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+	path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+
     path('about', views.about, name="blog-about"),
 ]
